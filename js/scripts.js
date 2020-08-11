b761014a4bf4f8ffe72e26d6710b14ba175065fe
Sean Wilson <paintballsean@gmail.com>
2020-08-11 12:00:37 -0700

    This commit will fix click here function in jQouery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#initially-showing").fadeOut();
-    $("#initially-hidden").fadeIn();  
+    $("#initially-showing").fadeToggle();
+    $("#initially-hidden").fadeToggle();  
   });
 });
